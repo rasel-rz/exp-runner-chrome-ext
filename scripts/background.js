41dc675af5a280ce2df66ead917df11ce49a2bf3
Raihan <md.raihanbdrasel@gmail.com>
2024-09-18 21:39:22 +0600

    Socket toggler, gotta figure a way out for the port settings

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,10 @@
 const activeStatus = {};
 const HTTPS_STORAGE_KEY = 'exp-runner-https';
+const NO_SOCKET_STORAGE_KEY = 'exp-runner-no-socket';
 
 chrome.action.onClicked.addListener(async (tab) => {
     activeStatus[tab.id] = !activeStatus[tab.id];
-    setLogo(activeStatus[tab.id], tab.id);
-    executeScript(tab.id);
+    activate(tab.id);
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
@@ -19,20 +19,37 @@ chrome.webNavigation.onCommitted.addListener(({ tabId, frameType }) => {
 
 chrome.commands.onCommand.addListener((command) => {
     if (command === 'switch-http-or-https') return toggleHttpOrHttps();
+    if (command === 'no-scoket-mode') return toggleNoSocketMode();
 });
 
 function activate(tabId) {
-    setHttpOrHttpsBadge().then(() => {
+    updateBadge().then(() => {
         setLogo(activeStatus[tabId], tabId);
         activeStatus[tabId] && executeScript(tabId);
+    });
+}
+
+function toggleNoSocketMode() {
+    chrome.storage.local.get(NO_SOCKET_STORAGE_KEY, (data) => {
+        const value = data[NO_SOCKET_STORAGE_KEY];
+        if (value === 'true') return chrome.storage.local.remove(NO_SOCKET_STORAGE_KEY, updateBadge);
+        chrome.storage.local.set({ [NO_SOCKET_STORAGE_KEY]: 'true' }, updateBadge);
+    });
+}
+
+function isNoSocketMode() {
+    return new Promise((resolve) => {
+        chrome.storage.local.get(NO_SOCKET_STORAGE_KEY, (data) => {
+            resolve(data[NO_SOCKET_STORAGE_KEY] === 'true');
+        });
     });
 }
 
 function toggleHttpOrHttps() {
     chrome.storage.local.get(HTTPS_STORAGE_KEY, (data) => {
         const value = data[HTTPS_STORAGE_KEY];
-        if (value === 'true') return chrome.storage.local.remove(HTTPS_STORAGE_KEY, setHttpOrHttpsBadge);
-        chrome.storage.local.set({ [HTTPS_STORAGE_KEY]: 'true' }, setHttpOrHttpsBadge);
+        if (value === 'true') return chrome.storage.local.remove(HTTPS_STORAGE_KEY, updateBadge);
+        chrome.storage.local.set({ [HTTPS_STORAGE_KEY]: 'true' }, updateBadge);
     });
 }
 
@@ -44,11 +61,11 @@ function isHttps() {
     });
 }
 
-function setHttpOrHttpsBadge() {
+function updateBadge() {
     return new Promise((resolve) => {
-        isHttps().then((isHttps) => {
-            chrome.action.setBadgeText({ text: (isHttps ? '://' : '') });
-            resolve(isHttps);
+        Promise.all([isHttps(), isNoSocketMode()]).then(([isHttps, isNoSocketMode]) => {
+            chrome.action.setBadgeText({ text: `${isHttps ? ':/' : ''}${isNoSocketMode ? '$' : ''}` });
+            resolve(true);
         });
     });
 }
@@ -62,19 +79,41 @@ function setLogo(isActive, tabId) {
 }
 
 function executeScript(tabId) {
-    isHttps().then((isHttps) => {
-        chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            func: (isHttps) => {
-                !(() => {
+    Promise.all([isHttps(), isNoSocketMode()]).then(([isHttps, isNoSocketMode]) => {
+        !isNoSocketMode &&
+            chrome.scripting.executeScript({
+                target: { tabId: tabId },
+                func: (isHttps) => {
+                    !(() => {
+                        if (window.experiment_runner_loaded) return;
+                        window.experiment_runner_loaded = true;
+                        const protocol = isHttps ? 'https' : 'http';
+                        const script = document.createElement("script");
+                        script.src = protocol + "://localhost:3001/experiment-runner.js";
+                        document.head.append(script);
+                    })();
+                },
+                args: [isHttps]
+            });
+
+        isNoSocketMode &&
+            chrome.scripting.executeScript({
+                target: { tabId: tabId },
+                func: (isHttps) => {
                     if (window.experiment_runner_loaded) return;
-                    const script = document.createElement("script");
-                    script.src = (isHttps ? 'https' : 'http') + "://localhost:3001/experiment-runner.js";
-                    document.head.append(script);
                     window.experiment_runner_loaded = true;
-                })();
-            },
-            args: [isHttps]
-        });
+                    const protocol = isHttps ? 'https' : 'http';
+                    const script = document.createElement("script");
+                    script.src = protocol + "://localhost:3001/variation.js";
+                    const style = document.createElement("link");
+                    style.rel = "stylesheet";
+                    style.href = protocol + "://localhost:3001/variation.css";
+                    document.head.append(script, style);
+                    script.onload = () => console.log("Experiment runner loaded without hot reload");
+                    return;
+
+                },
+                args: [isHttps]
+            });
     });
 }
